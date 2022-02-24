72c4c348a5d58d5fc49236b72673499376018c6c
Zerounary <850657663@qq.com>
2022-02-24 16:47:01 +0800

    feat(index): add subBetween function
    get string between before and after

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-module.exports = {
-  hi: () =>{
-    return 'Hi libs!'
+const INDEX_NOT_FOUND = -1;
+
+
+let subBetween = (str = "", before = "", after = "") => {
+  if (str === "" || (before === "" && after === "")) {
+    return str;
   }
-}
+
+  let start = str.indexOf(before);
+  if (start != INDEX_NOT_FOUND) {
+    if (after === "") {
+      return str.substring(start + before.length);
+    }
+    let end = str.indexOf(after, start + before.length);
+    if (end != INDEX_NOT_FOUND) {
+      return str.substring(start + before.length, end);
+    }else {
+      return str.substring(start + before.length);
+    }
+  }
+  return "";
+};
+
+module.exports = {
+  hi: () => {
+    return "Hi libs!";
+  },
+  subBetween,
+};
--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,37 @@ var lib = require("../src/index");
 
 describe("Hello test", function () {
   it("shoud be same string", function () {
-    expect(lib.hi()).to.eq("Hi libs!");
+    let result = lib.hi();
+    expect("Hi libs!").to.eq(result);
+  });
+  describe("subBetween", function () {
+    it("empty str return enpmty string", function () {
+      let result = lib.subBetween();
+      expect("").to.eq(result);
+    });
+    it("str not empty, the others is empty, return str", function () {
+      let result = lib.subBetween("The test string");
+      expect("The test string").to.eq(result);
+    });
+    it("str and before not empty, return str without before  ", function () {
+      let result = lib.subBetween("The test string", "The");
+      expect(" test string").to.eq(result);
+    });
+    it("str and end not empty, return str without end  ", function () {
+      let result = lib.subBetween("The test string", "", "ing");
+      expect("The test str").to.eq(result);
+    });
+    it("all not empty, return str between before and end  ", function () {
+      let result = lib.subBetween("The test string", "The", "ing");
+      expect(" test str").to.eq(result);
+    });
+    it("str before after is same string, return empty str", function () {
+      let result = lib.subBetween("1", "1", "1");
+      expect("").to.eq(result);
+    });
+    it("before not exist, after exist return empty str", function () {
+      let result = lib.subBetween("121", "3", "1");
+      expect("").to.eq(result);
+    });
   });
 });
