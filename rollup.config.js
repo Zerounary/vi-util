97b23a7375c278b8c308bd2fb75be2eac898418e
Zerounary <850657663@qq.com>
2022-02-25 11:27:05 +0800

    build(rollup): output umd cjs esm file

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,29 @@
+import filesize from "rollup-plugin-filesize";
+import {uglify} from "rollup-plugin-uglify";
+
 export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'umd'
-  }
-}
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "umd",
+    },
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "es",
+    },
+  ],
+  plugins: [
+    // terser()
+    uglify({
+      compress: {
+        drop_console: true,
+      },
+    }),
+    filesize()
+  ],
+};
